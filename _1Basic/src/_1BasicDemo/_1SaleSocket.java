837e42b19de0b6c6f673e98708cc47b79fddf187
liang <17629795912@163.com>
2020-03-15 16:16:20 +0800

    JUC

---
+++ _1Basic/src/_1BasicDemo/_1SaleSocket.java
@@ -0,0 +1,64 @@
+package _1BasicDemo;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Author:liang;
+ * Date:2020/3/15;
+ * Time:14:14;
+ * Package Name:_1BasicDemo;
+ * 需求：卖3000张票
+ * 步骤：
+ */
+class Ticket //资源类=实例变量+实例方法
+{
+    private int number=3000;
+    //ReentrantLock 是 可重用锁
+    Lock lock=new ReentrantLock();
+    public void sale()
+    {
+        lock.lock();
+        try{
+            if(number>0) {
+                System.out.println(Thread.currentThread().getName() + "卖出了第" + (number--) + "张票，现在还剩" + number);
+            }
+        }catch(Exception e)
+        {
+              e.printStackTrace();
+        }
+         finally {
+              lock.unlock();
+         }
+    }
+
+}
+
+/**
+ *  在高内聚低耦合的前提下   线程 + 操作 + 资源类
+ */
+public class _1SaleSocket {
+    public static void main(String[] args) {
+        Ticket ticket=new Ticket();
+        new Thread(()->{ for (int i = 0; i <4000 ; i++) ticket.sale(); },"A").start();//根据这个参数类型，它默认我这是runnable接口
+        new Thread(()->{ for (int i = 0; i <4000 ; i++) ticket.sale(); },"B").start();
+        new Thread(()->{ for (int i = 0; i <4000 ; i++) ticket.sale(); },"C").start();
+/*         new Thread(new Runnable() {
+           @Override
+           public void run() {
+               for (int i = 0; i < 4000; i++) {
+                   ticket.sale();
+               }
+           }
+       },"A").start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 4000; i++) {
+                    ticket.sale();
+                }
+            }
+        },"B").start();*/
+
+    }
+}
---
+++ _1Basic/src/_1BasicDemo/_2LambdaExpress.java
@@ -0,0 +1,51 @@
+package _1BasicDemo;
+
+/**
+ * Author:liang;
+ * Date:2020/3/15;
+ * Time:15:26;
+ * Package Name:_1BasicDemo;
+ * 需求：
+ * 步骤：
+ *
+ *
+ * 实现接口的高效形式
+ * 1.   Lambda表达式,当接口中只有一个方法时简便的写法，因为就一个方法肯定不会实现错，必须是一个方法。解决了匿名内部类代码冗余的情况.函数式接口才能用lambda
+ * 2.   小括号，箭头，大括号实现
+ * 3.   @FunctionalInterface显示声明函数式接口
+ * 4.   default 在函数式接口中可以实现多个 default函数
+ * 5.   static  可以实现多个static函数
+ * 6.   Java8技术
+ */
+
+
+//显示声明函数式接口
+@FunctionalInterface
+interface Foo{
+    public abstract int  add(int x,int y);
+    public default int mul(int x,int y)
+    {
+        return x*y;
+    }
+    public static int div(int x,int y)
+    {
+        return x/y;
+    }
+
+}
+public class _2LambdaExpress {
+    public static void main(String[] args) {
+        /*        Foo foo=new Foo() {
+            @Override
+            public void sayhello() {
+                System.out.println("hello.....");
+            }
+        };
+        foo.sayhello();*/
+        Foo foo=(int x,int y)->{
+            System.out.println("add method");
+            return x+y;
+        };
+        System.out.println(foo.add(3, 5));
+    }
+}
